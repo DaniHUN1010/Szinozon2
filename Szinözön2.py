91097616ea4fb35df8992bbae90d9c0324da252d
Szabó Dániel <szdanika12345@gmail.com>
2024-05-23 19:25:53 +0200

    idő javítása

--- Szinözön2.py
+++ Szinözön2.py
@@ -7,7 +7,7 @@ WIDTH, HEIGHT = 700, 850
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = pygame.time.Clock()
 BG = pygame.transform.scale(pygame.image.load("hatterkep.png"), (WIDTH, HEIGHT))
-FONT = pygame.font.SysFont("comicsans", 30)
+FONT = pygame.font.SysFont("Calibri", 30, bold = True)
 start_time = time.time()
 elapsed_time = 0
 pygame.display.set_caption("Színözön2")
@@ -15,9 +15,9 @@ pygame.display.set_caption("Színözön2")
 def draw(elapsed_time):
     WINDOW.blit(BG, (0, 0))
     pygame.display.update()
-    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    time_text = FONT.render(f"Idő: {round(elapsed_time)}s", 1, "white")
     WINDOW.blit(time_text, (10, 10))
-
+# Ha nyert vagy vesztett, akkor írja ki az időt! 
 def main():
     run = True
     while run:
