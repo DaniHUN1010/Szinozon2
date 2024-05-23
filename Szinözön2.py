b46b587fa04af5ed0ad4dcb9f00a049d906818bc
Dani <szdanika12345@gmail.com>
2024-05-23 10:08:30 +0200

    Háttérkép, fps lock to 60

--- Szinözön2.py
+++ Szinözön2.py
@@ -3,7 +3,13 @@ import random
 pygame.init()
 WIDTH, HEIGHT = 700, 1000
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = pygame.time.Clock()
+BG = pygame.transform.scale(pygame.image.load("placeholder.jpg"), (WIDTH, HEIGHT))
 pygame.display.set_caption("Színözön2")
+
+def draw():
+    WINDOW.blit(BG, (0, 0))
+    pygame.display.update()
 
 def main():
     run = True
@@ -12,6 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            
+        FPS.tick(60)
+        draw()
             
     pygame.quit()
     
