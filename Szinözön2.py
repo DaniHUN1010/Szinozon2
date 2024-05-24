f93e199b0d7759fb7db0de6ae7cba19451bdd8e2
tirhold-david <122042812+tirhold-david@users.noreply.github.com>
2024-05-24 09:25:47 +0200

    gomb helyek

--- Szinözön2.py
+++ Szinözön2.py
@@ -11,12 +11,16 @@ FONT = pygame.font.SysFont("Calibri", 30, bold = True)
 start_time = time.time()
 elapsed_time = 0
 pygame.display.set_caption("Színözön2")
-color = (200, 75, 75)
-rect = (50, 50, 310, 130)
+color = (186, 56, 56)
+rect_1 = (195, 440, 310, 130)  #középső gomb
+rect_2=(195, 270, 310, 130)    #felső gomb
+rect_3=(195, 610, 310, 130)    #alsó gomb
 
 def draw(elapsed_time):
     WINDOW.blit(BG, (0, 0))
-    pygame.draw.rect(WINDOW, color, rect)  # Téglalap rajzolása
+    pygame.draw.rect(WINDOW, color, rect_1)  # Téglalap rajzolása
+    pygame.draw.rect(WINDOW, color, rect_2)  # Téglalap rajzolása
+    pygame.draw.rect(WINDOW, color, rect_3)  # Téglalap rajzolása
     pygame.display.update()
     time_text = FONT.render(f"Idő: {round(elapsed_time)}s", 1, "white")
     WINDOW.blit(time_text, (10, 10))
